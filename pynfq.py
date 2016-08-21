c3240ed9675e25490b8faed01b86155accecb285
Gernot Vormayr <gvormayr@gmail.com>
2016-08-21 11:50:12 +0200

    Added initial python lib

--- pynfq.py
+++ pynfq.py
@@ -1,71 +1,55 @@
-from cffi import FFI
-ffibuilder = FFI()
+from _pynfq import ffi, lib
+import os
 
-ffibuilder.set_source("_pynfq",
-    """
-    #include "nfq_main.h"
-    """, libraries=["nfq"], library_dirs=['.'])
+#constants
 
-ffibuilder.cdef("""
-#define NF_DROP 0
-#define NF_ACCEPT 1
-#define NF_STOLEN 2
-#define NF_QUEUE 3
-#define NF_REPEAT 4
-#define NF_STOP 5
+class Packet:
+    def __init__(self, conn, p):
+        self.packet = p
+        self.conn = conn
 
-#define MANGLE_MARK    ...
-#define MANGLE_PAYLOAD ...
-#define MANGLE_CT      ...
-#define MANGLE_EXP     ...
-#define MANGLE_VLAN    ...
+    def verdict(self, action, mangle=0):
+        ret = lib.set_verdict(self.conn, self.packet[0], action, mangle)
+        if ret:
+            raise OSError(ret, 'Could not set packet verdict: ' + os.strerror(re))
+        lib.add_empty(self.conn, self.packet[0], 1) #invalidate packet
 
-#define NFQNL_COPY_NONE ...
-#define NFQNL_COPY_META ...
-#define NFQNL_COPY_PACKET ...
 
-#define NFQA_PAYLOAD ...
+#change to context manager!
+class Connection:
+    def __init__(self):
+        num = 10
+        size = 8192
+        self.conn = ffi.new("struct nfq_connection *");
+        lib.init_connection(self.conn, 0)
+        self.buffers = []
 
-struct nfq_attr {
-	void *buffer;
-	size_t len;
-	uint16_t type;
-};
+        self.packets = ffi.new("struct nfq_packet[]", num);
+        for i in range(num):
+            b = ffi.new("char []", 8192)
+            self.buffers.append(b)
+            self.packets[i].buffer = b
+            self.packets[i].len = 8192
+        lib.add_empty(self.conn, self.packets, num)
 
-struct nfq_packet {
-	void *buffer;
-	size_t len;
-	int error;
-	uint32_t  seq;
-	uint16_t queue_id;
-	uint32_t id;
-	struct nfq_attr attr[...];
-	...;
-};
+    def __iter__(self):
+        return self
 
-struct nfq_connection {
-    ...;
-};
+    def __next__(self):
+        p = ffi.new("struct nfq_packet * *")
+        lib.get_packet(self.conn, p, 1)
+        return Packet(self.conn, p)
+        
+    def bind(self, queue):
+        ret = lib.bind_queue(self.conn, queue)
+        if ret:
+            raise OSError(ret, 'Could not bind queue: ' + os.strerror(re))
 
-int send_msg(struct nfq_connection *conn, uint16_t id, uint16_t type,
-		struct nfq_attr *attr, int n);
-void parse_packet(struct msghdr *msg, struct nfq_packet *packet);
-void init_connection(struct nfq_connection *conn, int flags);
-void close_connection(struct nfq_connection *conn);
+    def set_mode(self, queue, size, mode):
+        ret = lib.set_mode(self.conn, queue, size, mode)
+        if ret:
+            raise OSError(ret, 'Could not change mode: ' + os.strerror(re))
 
-int bind_queue(struct nfq_connection *conn, uint16_t queue_id);
-int unbind_queue(struct nfq_connection *conn, uint16_t queue_id);
-int set_mode(struct nfq_connection *conn, uint16_t queue_id, uint32_t range,
-		uint8_t mode);
-//maxlen
-//flags
-//batch_verdict
+    def close(self):
+        lib.close_connection(self.conn)
 
-void add_empty(struct nfq_connection *conn, struct nfq_packet *packet, int n);
-int get_packet(struct nfq_connection *conn, struct nfq_packet **packet, int n);
-int set_verdict(struct nfq_connection *conn, struct nfq_packet *packet,
-		uint32_t verdict, uint32_t mangle);
-""")
-
-if __name__ == "__main__":
-    ffibuilder.compile(verbose=True)
--- test.py
+++ test.py
@@ -1,35 +1,20 @@
-from _pynfq import ffi, lib
+import pynfq
 from scapy.all import *
 
-num = 4
 queue = 1
 
-conn = ffi.new("struct nfq_connection *");
-packets = ffi.new("struct nfq_packet[]", num);
-buffers = []
+conn = pynfq.Connection()
 
-lib.init_connection(conn, 0)
+conn.bind(queue)
+conn.set_mode(queue, 1000, pynfq.lib.NFQNL_COPY_PACKET)
 
-for i in range(num):
-    b = ffi.new("char []", 8192)
-    buffers.append(b)
-    packets[i].buffer = b
-    packets[i].len = 8192
-
-lib.add_empty(conn, packets, num)
-
-print(lib.bind_queue(conn, queue))
-print(lib.set_mode(conn, queue, 1000, lib.NFQNL_COPY_PACKET))
-
-for i in range(100):
-    p = ffi.new("struct nfq_packet * *")
-    print(lib.get_packet(conn, p, 1))
-    buf = ffi.buffer(ffi.cast("char *", p[0].attr[lib.NFQA_PAYLOAD].buffer), p[0].attr[lib.NFQA_PAYLOAD].len)
+for packet in conn:
+    buf = pynfq.ffi.buffer(pynfq.ffi.cast("char *", packet.packet[0].attr[pynfq.lib.NFQA_PAYLOAD].buffer),
+            packet.packet[0].attr[pynfq.lib.NFQA_PAYLOAD].len)
     buf[79:84] = b'\x00'*5
     x = IP(bytes(buf))
     del x[ICMP].chksum
     buf[:] = bytes(x)
-    print(lib.set_verdict(conn, p[0], lib.NF_ACCEPT, lib.MANGLE_PAYLOAD))
-    lib.add_empty(conn, p[0], 1)
+    packet.verdict(pynfq.lib.NF_ACCEPT, pynfq.lib.MANGLE_PAYLOAD)
 
-lib.close_connection(conn)
+conn.close()
